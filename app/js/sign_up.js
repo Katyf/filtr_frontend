768631030b2fb01a5167789e314a542c9b4c75e8
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-03-15 17:22:16 -0400

    user ajax form is done

--- app/js/sign_up.js
+++ app/js/sign_up.js
@@ -6,14 +6,71 @@ var filtrApp = (function(){
 
   var init = function(){
     authToken = localStorage.getItem('authToken');
-    apiHost = 'https://filtr-api.herokuapp.com/posts/';
+    apiHost = 'https://filter-api.herokuapp.com/';
     setupAjaxRequests();
     $('#').on('submit', submitLogin);
     $('#').on('submit', submitRegistration);
     $('#').on('click', signOut);
   };
 
-  var submitRegistration = function(event)
+  var submitRegistration = function(event){
+    event.preventDefault();
+    $.ajax({
+      url: apiHost + '/users',
+      type: 'POST',
+      data: {user: {
+        email: $('#email').val(),
+        password: $('#password').val()
+      }},
+    }).done(loginSuccess).fail(function(){
+      console.log("error");
+    });
+    return false;
+  };
 
+  var loginSuccess = function(userData){
+    localStorage.setItem('authToken', userData.token);
+    console.log('Logged in!');
+    window.location.href = '/';
+    console.log('Logged in!');
+  };
 
+  var submitLogin = function(){
+    var $form;
+    event.preventDefault();
+    $form = $(this);
+    $.ajax({
+      url: apiHost + '/users/sign_in',
+      type: 'POST',
+      data: $form.serialize()
+    }).done(loginSuccess).fail(function(){
+      console.log('error');
+    });
+    return false;
+  };
+
+  var setupAjaxRequests = function(){
+    $.ajaxPrefilter(function(options){
+      options.headers = {};
+      options.headers['AUTHORIZATION'] = 'Token token='+ authToken;
+    });
+  };
+
+  var acceptFailure = function(error){
+    if(error.status === 401){
+      console.log('send to login screen');
+      window.location.href = '/sign_in.html';
+    }
+  };
+
+  var signOut = function(event){
+    event.preventDefault();
+    localStorage.removeItem('authToken');
+    authToken = undefined;
+  };
+  return {init: init};
+})();
+
+$(document).ready(function(){
+  filtrApp.init();
 });
