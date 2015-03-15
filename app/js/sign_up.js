f4a9aa041726413e4693271d7097d3f0c912ac6c
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-03-15 14:44:42 -0400

    working on ajax user request

--- app/js/sign_up.js
+++ app/js/sign_up.js
@@ -1,1 +1,19 @@
+/*global $:false*/
+'use strict';
 
+var filtrApp = (function(){
+  var authToken, apiHost;
+
+  var init = function(){
+    authToken = localStorage.getItem('authToken');
+    apiHost = 'https://filtr-api.herokuapp.com/posts/';
+    setupAjaxRequests();
+    $('#').on('submit', submitLogin);
+    $('#').on('submit', submitRegistration);
+    $('#').on('click', signOut);
+  };
+
+  var submitRegistration = function(event)
+
+
+});
